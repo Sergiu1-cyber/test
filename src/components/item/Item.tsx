141e8dfdbb261846e0cf203bf01df0bd1537d374
Sergiu1-cyber <68728921+Sergiu1-cyber@users.noreply.github.com>
2023-01-02 12:10:31 +0200

    1

--- src/components/item/Item.tsx
+++ src/components/item/Item.tsx
@@ -1,5 +1,4 @@
 import {IList} from "../../types/list"
-import classes from "./Item.module.css"
 
 interface IProps {
   item: IList
@@ -8,9 +7,13 @@ interface IProps {
 
 export default function Item({item, Remove}: IProps) {
   return (
-    <div className={classes.listContainer}>     
-      <h1>{item.title}</h1>
-      <button className={classes.listBtn} onClick={() => Remove(item.id)}>x</button>
+    <div className="w3-row w3-border w3-display-container"> 
+      <div className="w3-padding">
+        <h1>{item.title}</h1>
+      </div>    
+      <div className="w3-padding w3-display-right">
+        <button className="w3-btn w3-circle w3-red" onClick={() => Remove(item.id)}>x</button>
+      </div>
     </div>
   )
 }
